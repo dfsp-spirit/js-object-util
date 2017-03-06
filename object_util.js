904b3f308516bcd38259a0ab4e3016e96012a45c
dfsp-spirit <ts+code@rcmd.org>
2017-03-06 23:41:57 +0000

    remove unused debug code

--- object_util.js
+++ object_util.js
@@ -11,11 +11,11 @@ function ObjectUtil() {
 
 
 /**
- * Checks whether a nested object key exists in an object. Dives into arrays as well.
+ * Checks whether a nested object key exists in an object. Dives into arrays if required.
  * @see http://stackoverflow.com/questions/2631001/javascript-test-for-existence-of-nested-object-key
- * @param obj the JSON object in which to check for the nested key
- * @param path_components an array of strings defining the path (of key names) to the key in question
- * @returns {boolean} whether the key exists. (The value may still be null, undefined, or falsy.)
+ * @param obj The JSON object in which to check for the nested key
+ * @param path_components An array of strings and/or numbers defining the path (of key names) to the key in question. Numbers are interpreted as array indices.
+ * @returns {boolean} Whether the key exists. (The value may still be null, undefined, or falsy.)
  */
 ObjectUtil.hasIn = function(obj, path_components) {
 	let handled_path = [];
@@ -46,14 +46,11 @@ ObjectUtil.hasIn = function(obj, path_components) {
 			return false;   // must be object or array, and the latter is also of type object in JS
 		}
 		if(Array.isArray(obj)) {
-			//console.log("###### At " + handled_path.join(', ') + ": is array.");
 			if(ObjectUtil._stringRepresentsPositiveIntegerIncludingZero(path_component)) {
 				let index = parseInt(path_component);
-				//console.log("###### At " + handled_path.join(', ') + ": is array. index=" + index);
 				if(index >= 0 && index < obj.length) {
 					last_obj = obj;
 					obj = obj[index];
-					//console.log("###### At " + handled_path.join(', ') + ": is array. index=" + index + ". obj=%o", obj);
 				}
 				else {
 					return false;
